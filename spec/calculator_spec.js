14ae5c780d26332365d63818520adc063e13db14
John Mazzucco <John.Mazzucco@gmail.com>
2016-01-01 09:19:51 -0500

    initial commit

---
+++ spec/calculator_spec.js
@@ -0,0 +1,20 @@
+describe('operators', function() {
+	it('must add two numbers')
+});
+
+
+
+// Matcher examples:
+
+// expect('Hello World!').toEqual('Hello World!');
+// expect('Hello World!').not.toEqual('Goodbye!');
+// expect('Hello World!').toNotEqual('Hi!');
+// expect([1, 2, 3]).toEqual([1, 2, 3]);
+
+// expect(false).toBeFalsy();
+
+// expect(true).toBeTruthy();
+// expect({}).toBeDefined();
+
+// expect(null).toBeNull();
+// expect([1, 2, 3]).toContain(2);
